9b3a7923cf4ca7c7f9ae23a811c49146937a0d21
Jeffrey Moon <fourleaves@fourleaves.io>
2020-09-27 18:08:01 -0400

    still master

--- src/simconnectjs.cpp
+++ src/simconnectjs.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
-#include "addon.hpp"
+#include "simconnectjs.hpp"
 
-int add(int a, int b)
+Napi::Object SimConnectJs::Init(Napi::Env env, Napi::Object exports)
 {
-  return a + b;
+    Napi::Function func = DefineClass(env, "SimCononectJs",
+        { InstanceMethod("Open", &SimConnectJs::Open),
+            InstanceMethod("AddToDataDefinition", &SimConnectJs::AddToDataDefinition),
+            InstanceMethod("RequestDataOnSimObjectType", &Addon::RequestDataOnSimObjectType) });
+
+    Napi::FunctionReference* constructor = new Napi::FunctionReference();
+    *constructor = Napi::Persistent(func);
+    env.SetInstanceData(constructor);
+    exports.Set("SimConnectJs", func);
+    return exports;
+}
+
+SimConnectJs::SimConnectJs(const Napi::CallbackInfo& info)
+    : Napi::ObjectWrap<Addon>(info)
+{
+}
+
+Napi::Boolean SimConnectJs::Open(Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+    std::string name = info[0].As<Napi::String>().Utf8Value();
+    HRESULT result = SimConnect_Open(&this->fsHandle, name, NULL, 0, NULL, 0);
+
+    if (result == 0) {
+        return Napi::Boolean::New(env, true);
+    } else {
+        return Napi::Boolean::New(env, false);
+    }
 }
--- src/simconnectjs.hpp
+++ src/simconnectjs.hpp
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "napi.h"
 #include <SimConnect.h>
 
+class SimConnectJs {
 public:
+    static Napi::Object Init(Napi::Env env, Napi::Object exports);
+    SimConnectJs(const Napi::CallbackInfo& info);
+
 private:
+    HANDLE fsHandle;
+    Napi::Boolean Open(Napi::CallbackInfo& info);
+    Napi::Boolean AddToDataDefinition(Napi::CallbackInfo& info);
+    Napi::Boolean RequestDataOnSimObjectType(Napi::CallbackInfo& info);
+}
